1acd61075978f9fa551692563ce5cacaca17b5eb
Anvar <torczon@localhost.localdomain>
2018-01-29 14:33:58 +0530

    add CharacterButton

---
+++ charbutton.cpp
@@ -0,0 +1,6 @@
+#include "charbutton.h"
+
+CharButton::CharButton()
+{
+
+}
---
+++ charbutton.h
@@ -0,0 +1,11 @@
+#ifndef CHARBUTTON_H
+#define CHARBUTTON_H
+
+
+class CharButton
+{
+public:
+    CharButton();
+};
+
+#endif // CHARBUTTON_H
